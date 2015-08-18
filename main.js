02811942cea56c8d7ce73d3cc93a5de541b23fe3
Niall Wallace <nj.wallace@outlook.com>
2015-08-18 21:12:03 +0100

    added checkLine() function

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var Subway = {
-      'n': ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'],
-      'l': ['8th', '6th', 'Union Square', '3rd', '1st'],
-      '6': ['Grand Central', '33rd', '6-28th', '6-23rd', 'Union Square', 'Astor Place']
+      'n': ['N-Times Square', 'N-34th', 'N-28th', 'N-23rd', 'Union Square', 'N-8th'],
+      'l': ['L-8th', 'L-6th', 'Union Square', 'L-3rd', 'L-1st'],
+      'six' : ['6-Grand Central', '6-33rd', '6-28th', '6-23rd', 'Union Square', '6-Astor Place']
   }
-
+var destinationLine;
 var start = document.getElementById('startPoint');
 var end = document.getElementById('endPoint');
 
@@ -11,23 +11,19 @@ end.addEventListener('change', function(e){
   startIndex = Subway.n.indexOf(start.value);
   endIndex = Subway.n.indexOf(end.value);
 
-    if(endIndex == 0){
   var result = startIndex - endIndex;
   result = Math.abs(result);
   document.getElementById('answer').innerHTML = result;
-
+  checkLine(end);
   console.log(startIndex);
   console.log(endIndex);
   console.log('result ' + result);
-}
-})
 
-function changeSubway(destination) {
-  var destLine
-if (Subway.n.contains(destination) === true) {
-  destLine = 'l';
-  console.log(destLine);
+});
 
-}
+function checkLine(location){
+  line = location.value.slice(0,1).toLowerCase();
+console.log(line);
+  }
 
-}
+
